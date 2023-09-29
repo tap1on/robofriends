112c4ab0d378d758b05d5a34097b8c1e507ae741
Calle Sjostrom <ch@leadcapdigital.com>
2023-09-29 12:02:23 +0200

    Updates

---
+++ precache-manifest.330a3eccb70852f51fe936044e29cb93.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "bea8e45d8e7cbd2a560a",
+    "url": "/robofriends/static/css/main.37a09563.chunk.css"
+  },
+  {
+    "revision": "bea8e45d8e7cbd2a560a",
+    "url": "/robofriends/static/js/main.bea8e45d.chunk.js"
+  },
+  {
+    "revision": "ee2fa2fc286181834050",
+    "url": "/robofriends/static/js/1.ee2fa2fc.chunk.js"
+  },
+  {
+    "revision": "024c8c04e6023dc84370",
+    "url": "/robofriends/static/css/2.3f85d9e5.chunk.css"
+  },
+  {
+    "revision": "024c8c04e6023dc84370",
+    "url": "/robofriends/static/js/2.024c8c04.chunk.js"
+  },
+  {
+    "revision": "0e09ba9c7502e90c21a0",
+    "url": "/robofriends/static/js/runtime~main.0e09ba9c.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "d9ece81e4a52e4a8a7324bee4c82b99f",
+    "url": "/robofriends/index.html"
+  }
+];
